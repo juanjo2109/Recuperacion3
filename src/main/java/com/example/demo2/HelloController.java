69eade4d8a6b93bf0303f9efaf326e066f17d50c
juanjo2109 <juanjoalfereez@gmail.com>
2023-04-21 11:39:45 +0200

    Res3

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -1,14 +1,81 @@
 package com.example.demo2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private final String usuario = "adminer";
+    private final String passwd = "adminer";
+    private final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
 
+    private Connection conexionBBDD;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField ne;
+    @FXML
+    private TextField apellido;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField extel;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField codof;
+    @FXML
+    private TextField respo;
+    @FXML
+    private TextField trabajo;
+    @FXML
+    private Button alta;
+    @FXML
+    private Button salida;
+
+    public void altaEmpleado() {
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "INSERT INTO employees ("
+                    + "  employeeNumber,"
+                    + "  lastName,"
+                    + "  firstName,"
+                    + "  extension,"
+                    + "  email,"
+                    + "  officeCode,"
+                    + "  reportsTo,"
+                    + "  jobTitle)"
+                    + " VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement st = conexionBBDD.prepareStatement(SQL);
+            st.setInt(1, Integer.parseInt(ne.getText()));
+            st.setString(2, apellido.getText() );
+            st.setString(3, nombre.getText() );
+            st.setString(4, extel.getText());
+            st.setString(5, email.getText());
+            st.setString(6, codof.getText() );
+
+            st.setInt(7, Integer.parseInt(respo.getText()));
+            st.setString(8, trabajo.getText());
+
+
+            // Ejecutamos la consulta preparada (con las ventajas de seguridad y velocidad en el servidor de BBDD
+            // nos devuelve el número de registros afectados. Al ser un Insert nos debe devolver 1 si se ha hecho correctamente
+            st.executeUpdate();
+            st.close();
+            conexionBBDD.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+        }
     }
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.demo2 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.mariadb.jdbc;
+    requires java.sql;
 
     opens com.example.demo2 to javafx.fxml;
     exports com.example.demo2;
