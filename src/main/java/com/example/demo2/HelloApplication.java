780c837a0579319fe346e65c0b8723f7b8429fac
juanjo2109 <juanjoalfereez@gmail.com>
2023-06-19 18:17:54 +0200

    Mod13

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("empleado-vista.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 666, 400);
         stage.setTitle("Tabla Principal");
         stage.setScene(scene);
         stage.show();
